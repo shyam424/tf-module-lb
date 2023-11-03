bfff8db7c8cb1f92ec8a1c1ceaef332e9aa26f14
Shyam <shyamneo24@gmail.com>
2023-11-03 13:34:26 +0530

    vpc

--- locals.tf
+++ locals.tf
@@ -2,4 +2,5 @@ locals {
   #TAGS
   tags = merge (var.tags, {tf-module-name = "lb"}, {env = var.env} )
   sg_name = var.internal ? "${var.env}-lb-internal-sg" : "${var.env}-lb-public-sg"
+  lb_name = var.internal ? "${var.env}-lb-internal" : "${var.env}-lb-public"
 }
